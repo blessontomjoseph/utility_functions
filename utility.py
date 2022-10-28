7b8c4973ec5684d51396e0ac41669ea38a9495fe
tom <57592961+blessontomjoseph@users.noreply.github.com>
2022-10-28 13:27:29 +0530

    Update utility.py

--- utility.py
+++ utility.py
@@ -1,1 +1,13 @@
+def diagnostic_plots(df, variable):
+    # function to plot a histogram and a Q-Q plot
+    # side by side, for a certain variable
+    
+    plt.figure(figsize=(15,6))
+    plt.subplot(1, 2, 1)
+    df[variable].hist()
 
+    plt.subplot(1, 2, 2)
+    stats.probplot(df[variable], dist="norm", plot=plt)
+
+    plt.show()
+    
